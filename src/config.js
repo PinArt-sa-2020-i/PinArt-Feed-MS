1d6e6b18b7780cb4a6f4ecc08ea157d388f62c02
CamiloBallen24 <andrescamilo468@gmail.com>
2020-06-24 17:39:09 -0500

    rancher ip two

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'ec2-3-235-182-65.compute-1.amazonaws.com:27017',
+    db: process.env.MONGODB || 'ec2-34-204-194-146.compute-1.amazonaws.com:27017',
     SECRET_TOKEN: 'starkteampinart'
 }
